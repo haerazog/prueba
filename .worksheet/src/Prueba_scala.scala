d78be58a0325705715acf6cae8b19156d68dba18
haerazog <haerazog@DESKTOP-9UOSH1O>
2019-03-09 22:30:37 -0500

    Esta es una prueba de cómo crear un respositorio en Eclipse

---
+++ .worksheet/src/Prueba_scala.scala
@@ -0,0 +1,6 @@
+object Prueba_scala {;import org.scalaide.worksheet.runtime.library.WorksheetSupport._; def main(args: Array[String])=$execute{;$skip(57); 
+	val numeros = List(45, 43, 56, 75);System.out.println("""numeros  : List[Int] = """ + $show(numeros ));$skip(23); val res$0 = 
+	numeros.map(x => _*2);System.out.println("""res0: <error> = """ + $show(res$0))}
+
+
+}
---
+++ bin/Prueba_scala.sc
@@ -0,0 +1,6 @@
+object Prueba_scala {
+	val numeros = List(45, 43, 56, 75)
+	numeros.map(x => _*2)
+
+
+}
